e229d7fcb9b44ad443dfb580c471ce0fc25f38f9
Artur Sattelmaier <dubwoof@me.com>
2023-10-11 15:43:49 +0200

    style fix

--- src/sections/HomeSection/HomeSection.styles.ts
+++ src/sections/HomeSection/HomeSection.styles.ts
@@ -1,5 +1,5 @@
 export const styles = {
-    section: 'w-[1000px] mx-auto h-screen flex justify-center items-center',
+    section: 'w-[1100px] mx-auto h-screen flex justify-center items-center',
     content: 'flex gap-4 w-full bg-purple-100',
     title: 'text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 mb-4',
     tryButton: 'bg-primary text-white font-bold py-2 px-4 rounded-full cursor-pointer w-max',
--- src/sections/HomeSection/HomeSection.tsx
+++ src/sections/HomeSection/HomeSection.tsx
@@ -10,8 +10,8 @@ import Link from 'next/link';
 export function HomeSection({ getRef }: HomeSectionProps): JSX.Element {
     return (
         <section id={Sections.Home} ref={getRef} className={styles.section} data-style="section">
-            <div className="flex w-full">
-                <div className="flex flex-col w-1/2">
+            <div className="flex w-full gap-4">
+                <div className="flex flex-col w-2/5">
                     <Typography variant="headline" className="text-6xl mb-4">
                         Frontend Features, Masterfully Crafted.
                     </Typography>
@@ -20,7 +20,9 @@ export function HomeSection({ getRef }: HomeSectionProps): JSX.Element {
                         <br />
                         Future-Ready
                         <br />
-                        Micro Frontends for global Enterprises.
+                        Micro Frontends
+                        <br />
+                        for global Enterprises.
                     </h1>
                     <Typography variant="caption" className="mb-4">
                         Revolutionizing enterprise digital landscapes with cutting-edge micro frontends, we deliver scalable, innovative, and bespoke solutions,
@@ -31,7 +33,7 @@ export function HomeSection({ getRef }: HomeSectionProps): JSX.Element {
                         <div className={styles.tryButton}>Try it free</div>
                     </Link>
                 </div>
-                <div className="w-1/2">
+                <div className="w-3/5">
                     <div className="w-full h-full bg-microFrontends rounded-3xl bg-cover"></div>
                 </div>
             </div>
