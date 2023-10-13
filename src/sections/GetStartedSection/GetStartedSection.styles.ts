78cabf7d66f52915493ee7fc67d37e43070ffbe8
Artur Sattelmaier <dubwoof@me.com>
2023-10-13 16:26:25 +0200

    style

--- src/sections/GetStartedSection/GetStartedSection.styles.ts
+++ src/sections/GetStartedSection/GetStartedSection.styles.ts
@@ -2,7 +2,8 @@ export const styles = {
     section: 'w-[1100px] mx-auto h-screen flex justify-center items-start',
     content: 'flex gap-4 w-full bg-purple-100',
     title: 'text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 mb-4',
-    tabSelect: (isActive: boolean) => {
-        return `hover:bg-background h-10 px-2 flex items-center cursor-pointer ${isActive ? 'text-primary' : 'text-text'}`;
-    },
 };
+
+export function tabSelect(isActive: boolean) {
+    return `hover:bg-background h-12 px-2 flex items-center cursor-pointer ${isActive ? 'text-primary' : 'text-text'}`;
+}
--- src/sections/GetStartedSection/GetStartedSection.tsx
+++ src/sections/GetStartedSection/GetStartedSection.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { styles } from './GetStartedSection.styles';
+import { styles, tabSelect } from './GetStartedSection.styles';
 import { Sections } from '@/enums/Sections';
 import { GetstartedSectionProps } from './GetStartedSection.props';
 import { Sandbox } from '@/components/Sandbox/Sandbox';
@@ -21,15 +21,15 @@ export function GetStartedSection({ getRef }: GetstartedSectionProps): JSX.Eleme
         <section id={Sections.GetStarted} ref={getRef} className={styles.section} data-style="section">
             <div className="flex flex-col w-full items-center justify-start">
                 <h1 className={styles.title}>Get Started</h1>
-                <div className="flex w-full bg-card/70 backdrop-blur-3xl rounded-3xl border-[1px] border-background shadow-md">
+                <div className="flex w-full bg-slate-100/40 backdrop-blur-3xl rounded-3xl border-[1px] border-background shadow-md">
                     <div className="flex flex-col w-[350px] h-fit">
-                        <div onClick={() => handleTabClick('iframe')} className={styles.tabSelect(activeTab === 'iframe')}>
+                        <div onClick={() => handleTabClick('iframe')} className={tabSelect(activeTab === 'iframe')}>
                             <Typography variant="strong">iFrame</Typography>
                         </div>
-                        <div onClick={() => handleTabClick('react')} className={styles.tabSelect(activeTab === 'react')}>
+                        <div onClick={() => handleTabClick('react')} className={tabSelect(activeTab === 'react')}>
                             <Typography variant="strong">React (npm)</Typography>
                         </div>
-                        <div onClick={() => handleTabClick('moduleFederation')} className={styles.tabSelect(activeTab === 'moduleFederation')}>
+                        <div onClick={() => handleTabClick('moduleFederation')} className={tabSelect(activeTab === 'moduleFederation')}>
                             <Typography variant="strong">Module Federation</Typography>
                         </div>
                     </div>
